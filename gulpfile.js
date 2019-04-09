a34873d501d2a2239a2166f8651104edf23c831b
zhoujin <zhoujin@iboxpay.com>
2019-04-09 09:11:12 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp  = require('gulp')
+var gulp = require('gulp')
 var minifycss = require('gulp-minify-css')
 // var jshint = require('gulp-jshint')
 var uglify = require('gulp-uglify')
@@ -12,27 +12,46 @@ var cache = require('gulp-cache')
 // var htmlmin = require('gulp-htm')
 var autoprefixer = require('gulp-autoprefixer')
 var babel = require('gulp-babel')
+var rev = require('gulp-rev')
+var reCollector = require('gulp-rev-collector')
 
-
-gulp.task('scripts',function(){
-    return gulp.src(['src/js/*.js'])
-      .pipe(babel({
-        presets:['env']
-      }))
-        // .pipe(uglify())
-        .pipe(gulp.dest('src/dist/js'))
-        .pipe(notity({
-            message:'Scripts task complete'
-        }))
+gulp.task('scripts', function() {
+  return gulp.src(['src/js/*.js']).pipe(
+    babel({
+      presets: ['env']
+    })
+  )
+  pipe(rev())
+    .pipe(gulp.dest('src/dist/js'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/js'))
+    .pipe(
+      notity({
+        message: 'Scripts task complete'
+      })
+    )
 })
 
-gulp.task('clean',function(){
-    return gulp.src(['src/dist'],{
-        read:false
+gulp.task('html', function() {
+  gulp.src(['rev/**/*.json', 'src/*.html']).pipe(
+    reCollector({
+      replaceReved: true,
+      dirReplacements: {
+        'css/': '/dist/css/',
+        'js/': '/dist/js/'
+      }
+    }).pipe(gulp.dest('dist/view'))
+  )
+})
+
+gulp.task('clean', function() {
+  return gulp
+    .src(['src/dist'], {
+      read: false
     })
     .pipe(clean())
 })
 
-gulp.task('default',['clean'],function(){
-    gulp.run(['scripts'])
+gulp.task('default', ['clean'], function() {
+  gulp.run(['scripts', 'html'])
 })
